3416bbd508176b171ed5ada3484ffe2e54d453d4
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-02-12 17:50:02 +0000

    Added main function, imported math library

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Jack Turcotte
+# Date: February 12, 2025
+# This program performs basic math operations
+
+import math
+
+
+def main():
+    print("0.5 + 10 ={}".format(0.5 + 10))  # Addition
+    print("2 - 6 ={}".format(2 - 6))  # Subtraction
+    print("5 * 5 ={}".format(5 * 5))  # Multiplication
+    print("4 / 5 ={}".format(4 / 5))  # Division
+    print("2 ** 15 ={}".format(2**15))  # Exponent
+    print("root 25 ={}".format(math.sqrt(25)))  # Square root
+    print("123%11 ={}".format(122 % 11))  # Remainder
+
+
+if __name__ == "__main__":
+    main()
